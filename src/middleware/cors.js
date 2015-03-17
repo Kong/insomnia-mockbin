2bc4e649fc7f3ca6ca2053044ef4e16d5666b1e9
Ahmad Nassri <ahmad@codeinchaos.com>
2015-03-17 10:45:53 -0700

    allow cors headers

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -4,7 +4,8 @@ module.exports = function (req, res, next) {
   res.set({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*'
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Credentials': 'true'
   });
 
   next();

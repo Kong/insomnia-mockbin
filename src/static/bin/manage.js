cd50f38b80c764e0662bc994b2952feee17559bc
Robert Wlaschin <robert.wlaschin@rockyou.com>
2018-01-02 10:48:44 -0800

    ADS-1133 : Working on adding delete checks and dialog

--- src/static/bin/manage.js
+++ src/static/bin/manage.js
@@ -4,5 +4,25 @@
 $(function () {
   $(document).ready(function () {
     console.log('Loaded the manage page')
+
+    // all button delete icons
+    $('i.delete').each(function(item) {
+      var $elem = $(item)
+      var $locked = $elem.parent().parent().siblings('td div i.fa.lock').find('div i.fa.lock')
+      $elem.on('click',function(event) {
+debugger
+        // If I'm locked display the modal
+        // if not delete with redir
+        if( $locked.hasClass('fa-lock') ) {
+          // set the button target to 
+          // $elem.src
+          $('#locked_warning').find('#warning-delete').attr('href',$elem.src)
+          $('#locked_warning').modal('show')
+        } else {
+          alert('Deleting!!', $elem.src)
+          // document.location= $elem.src
+        }
+      }.bind($elem))
+    })
   })
 })

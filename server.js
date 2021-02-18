ede26ec137942187274d7c94e369f64113c5c6d7
yashjain1974 <69360295+yashjain1974@users.noreply.github.com>
2021-02-18 17:34:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,26 @@ var dotenv = require('dotenv')
 var pkg = require('./package')
 
 dotenv.config({ silent: true })
+try {
+    app(options, function () {
+  console.info('starting server on port: %d', options.port)
+})
 
-var options = {
+  }
+  catch(err) {
+    message.innerHTML = "Input " + err;
+  }
+}
+try {
+   var options = {
   port: process.env.MOCKBIN_PORT || pkg.config.port,
   quiet: process.env.MOCKBIN_QUIET || pkg.config.quiet,
   redis: process.env.MOCKBIN_REDIS || pkg.config.redis
 }
 
-app(options, function () {
-  console.info('starting server on port: %d', options.port)
-})
+  }
+  catch(err) {
+    message.innerHTML = "Input " + err;
+  }
+}
+

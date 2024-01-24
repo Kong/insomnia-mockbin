9222ae62968a282fb7833c4aa1cccc815a68bcc2
Jack Kavanagh <jackkav@gmail.com>
2024-01-24 10:29:35 +0100

    update dot env (#159)

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const app = require("./src");
 const dotenv = require("dotenv");
 
-const result = dotenv.config({ silent: false });
-if (result.error) {
-	console.warn(`
-	------------------------
-	Missing env file:
-	run this to fix it.
-	cp .env.sample .env
-	------------------------
-	`);
-	throw result.error;
-}
+dotenv.config();
 
 const options = {
 	port: process.env.MOCKBIN_PORT,

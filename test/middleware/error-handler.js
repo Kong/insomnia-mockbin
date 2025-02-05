d54b2ace14dea70c806f2ba7c5ed2f3b593c3534
Animesh Pathak <kurosakiichigo.songoku@gmail.com>
2025-02-05 18:59:16 +0530

    feat: use biome
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>

--- test/middleware/error-handler.js
+++ test/middleware/error-handler.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 
-const errorHandler = require('../../lib/middleware/error-handler');
-const httpMocks = require('node-mocks-http');
-const sinon = require('sinon');
+const errorHandler = require("../../lib/middleware/error-handler");
+const httpMocks = require("node-mocks-http");
+const sinon = require("sinon");
 
 let expect;
 
 // Test generated using Keploy
 
 before(async () => {
-  const chai = await import('chai');
-  expect = chai.expect;
+	const chai = await import("chai");
+	expect = chai.expect;
 });
 
-describe('Error Handler Middleware', () => {
-  it('should set status to 500 and view to "error" when no status is provided', () => {
-    const req = httpMocks.createRequest();
-    const res = httpMocks.createResponse();
-    const next = sinon.spy();
-    
-    const error = new Error('Test error');
-    
-    errorHandler(error, req, res, next);
-    
-    expect(res.statusCode).to.equal(500);
-    expect(res.view).to.equal('error');
-    expect(next.called).to.be.true;
-  });
+describe("Error Handler Middleware", () => {
+	it('should set status to 500 and view to "error" when no status is provided', () => {
+		const req = httpMocks.createRequest();
+		const res = httpMocks.createResponse();
+		const next = sinon.spy();
+
+		const error = new Error("Test error");
+
+		errorHandler(error, req, res, next);
+
+		expect(res.statusCode).to.equal(500);
+		expect(res.view).to.equal("error");
+		expect(next.called).to.be.true;
+	});
 });
--- test/routes/cookies.js
+++ test/routes/cookies.js
@@ -5,129 +5,129 @@ const httpMocks = require("node-mocks-http");
 require("should");
 
 describe("/cookie/:name", () => {
-  it("should response with one cookie", (done) => {
-    const res = {};
-    const req = {
-      params: { 
-		name: "foo" 
-		},
-      cookies: { 
-		foo: "bar" 
-		},
-    };
+	it("should response with one cookie", (done) => {
+		const res = {};
+		const req = {
+			params: {
+				name: "foo",
+			},
+			cookies: {
+				foo: "bar",
+			},
+		};
 
-    cookies.one(req, res, () => {
-      res.body.should.equal(req.cookies.foo);
-      done();
-    });
-  });
+		cookies.one(req, res, () => {
+			res.body.should.equal(req.cookies.foo);
+			done();
+		});
+	});
 
-  it("should response with false when no cookies are defined", (done) => {
-    const res = {};
-    const req = {
-      params: { name: "foo" },
-      cookies: {},
-    };
+	it("should response with false when no cookies are defined", (done) => {
+		const res = {};
+		const req = {
+			params: { name: "foo" },
+			cookies: {},
+		};
 
-    cookies.one(req, res, () => {
-      res.body.should.be.false();
-      done();
-    });
-  });
+		cookies.one(req, res, () => {
+			res.body.should.be.false();
+			done();
+		});
+	});
 
-  it("should response with false when no match", (done) => {
-    const res = {};
-    const req = {
-      params: { name: "foo" },
-    };
+	it("should response with false when no match", (done) => {
+		const res = {};
+		const req = {
+			params: { name: "foo" },
+		};
 
-    cookies.one(req, res, () => {
-      res.body.should.be.false();
-      done();
-    });
-  });
+		cookies.one(req, res, () => {
+			res.body.should.be.false();
+			done();
+		});
+	});
 
-  it("should return the value of the existing cookie", (done) => {
-    const req = httpMocks.createRequest({
-      params: { name: "testcookie" },
-      cookies: { testcookie: "testValue" },
-    });
-    const res = httpMocks.createResponse();
-    const next = () => {
-      res.body.should.equal("testValue");
-      done();
-    };
-    cookies.one(req, res, next);
-  });
+	it("should return the value of the existing cookie", (done) => {
+		const req = httpMocks.createRequest({
+			params: { name: "testcookie" },
+			cookies: { testcookie: "testValue" },
+		});
+		const res = httpMocks.createResponse();
+		const next = () => {
+			res.body.should.equal("testValue");
+			done();
+		};
+		cookies.one(req, res, next);
+	});
 });
 
 describe("/cookie/:name", () => {
-  it("should response with all cookies", (done) => {
-    const res = {};
-    const req = {
-      har: {
-        log: {
-          entries: [
-            {
-              request: {
-                cookies: ["test"],
-              },
-            },
-          ],
-        },
-      },
-    };
+	it("should response with all cookies", (done) => {
+		const res = {};
+		const req = {
+			har: {
+				log: {
+					entries: [
+						{
+							request: {
+								cookies: ["test"],
+							},
+						},
+					],
+				},
+			},
+		};
 
-    cookies.all(req, res, () => {
-      res.body.should.equal(req.har.log.entries[0].request.cookies);
-      done();
-    });
-  });
+		cookies.all(req, res, () => {
+			res.body.should.equal(req.har.log.entries[0].request.cookies);
+			done();
+		});
+	});
 
-  // Test generated using Keploy
+	// Test generated using Keploy
 
-  it("should retrieve all cookies from the HAR log", (done) => {
-    const req = httpMocks.createRequest({
-      har: {
-        log: {
-          entries: [
-            {
-              request: {
-                cookies: [
-                  { name: "cookie1", value: "value1" },
-                  { name: "cookie2", value: "value2" },
-                ],
-              },
-            },
-          ],
-        },
-      },
-    });
-    const res = httpMocks.createResponse();
-    const next = () => {
-      res.body.should.deepEqual([
-        { name: "cookie1", value: "value1" },
-        { name: "cookie2", value: "value2" },
-      ]);
-      done();
-    };
-    cookies.all(req, res, next);
-  });
+	it("should retrieve all cookies from the HAR log", (done) => {
+		const req = httpMocks.createRequest({
+			har: {
+				log: {
+					entries: [
+						{
+							request: {
+								cookies: [
+									{ name: "cookie1", value: "value1" },
+									{ name: "cookie2", value: "value2" },
+								],
+							},
+						},
+					],
+				},
+			},
+		});
+		const res = httpMocks.createResponse();
+		const next = () => {
+			res.body.should.deepEqual([
+				{ name: "cookie1", value: "value1" },
+				{ name: "cookie2", value: "value2" },
+			]);
+			done();
+		};
+		cookies.all(req, res, next);
+	});
 
-  // Test generated using Keploy
+	// Test generated using Keploy
 
-  it("should set a cookie with the correct name and value", (done) => {
-    const req = httpMocks.createRequest({
-      params: { name: "newCookie", value: "newValue" },
-    });
-    const res = httpMocks.createResponse();
-    res.cookie = function (name, value) {
-      this.body = value;
-    };
-    const next = () => {
-      res.body.should.equal("newValue");
-      done();
-    };
-    cookies.set(req, res, next);
-  });
+	it("should set a cookie with the correct name and value", (done) => {
+		const req = httpMocks.createRequest({
+			params: { name: "newCookie", value: "newValue" },
+		});
+		const res = httpMocks.createResponse();
+		res.cookie = function (name, value) {
+			this.body = value;
+		};
+		const next = () => {
+			res.body.should.equal("newValue");
+			done();
+		};
+		cookies.set(req, res, next);
+	});
 });

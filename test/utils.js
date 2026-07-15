2f569607c42c6ab9644d5e001ae3c9e6c05a4d6d
shelby-moore <13246465+shelby-moore@users.noreply.github.com>
2026-07-15 16:35:52 -0700

    chore: expand allowed chars in mock url paths

--- test/utils.js
+++ test/utils.js
@@ -70,6 +70,98 @@ describe("Utils", () => {
 		});
 	});
 
+	describe("isValidCompoundId", () => {
+		const validId = `mock_${"a".repeat(32)}`;
+
+		it("should return null for a valid id and basic path", (done) => {
+			const result = utils.isValidCompoundId(validId, "/foo/bar_baz-qux~.txt");
+
+			should(result).be.exactly(null);
+
+			done();
+		});
+
+		it("should return null for a path with sub-delim characters", (done) => {
+			const result = utils.isValidCompoundId(
+				validId,
+				"/foo;bar=baz,qux!$&'()*+",
+			);
+
+			should(result).be.exactly(null);
+
+			done();
+		});
+
+		it("should return null for a path with `:` and `@`", (done) => {
+			const result = utils.isValidCompoundId(validId, "/foo:8080/@user");
+
+			should(result).be.exactly(null);
+
+			done();
+		});
+
+		it("should return null for a path containing `..`", (done) => {
+			const result = utils.isValidCompoundId(validId, "/foo/../bar");
+
+			should(result).be.exactly(null);
+
+			done();
+		});
+
+		it("should return an error for a path containing `%`", (done) => {
+			const result = utils.isValidCompoundId(validId, "/foo%2Fbar");
+
+			result.should.be.an.Object();
+			result.error.should.be.equal("Invalid Path Characters");
+
+			done();
+		});
+
+		it("should return an error for a path containing a space", (done) => {
+			const result = utils.isValidCompoundId(validId, "/foo bar");
+
+			result.should.be.an.Object();
+			result.error.should.be.equal("Invalid Path Characters");
+
+			done();
+		});
+
+		it("should return an error for a path containing `?`", (done) => {
+			const result = utils.isValidCompoundId(validId, "/foo?bar=1");
+
+			result.should.be.an.Object();
+			result.error.should.be.equal("Invalid Path Characters");
+
+			done();
+		});
+
+		it("should return an error for a malformed id", (done) => {
+			const result = utils.isValidCompoundId("not_a_valid_id", "/foo");
+
+			result.should.be.an.Object();
+			result.error.should.be.equal('Invalid Mock "id"');
+
+			done();
+		});
+
+		it("should return null for a path of exactly 1900 characters", (done) => {
+			const result = utils.isValidCompoundId(validId, `/${"a".repeat(1899)}`);
+
+			should(result).be.exactly(null);
+
+			done();
+		});
+
+		it("should return an error for a path of exactly 1901 characters", (done) => {
+			const result = utils.isValidCompoundId(validId, `/${"a".repeat(1900)}`);
+
+			result.should.be.an.Object();
+			result.error.should.be.equal('Invalid Mock "path" length');
+
+			done();
+		});
+	});
+
 	describe("createHar", () => {
 		const result = utils.createHar(fixture);
 
